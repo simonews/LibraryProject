4939cea34b3d20e251b327128f0c7219e13af221
simonews <simonegoffredows@gmail.com>
2025-07-07 16:50:28 +0200

    Aggiunta main per il primo run dell'applicazione

---
+++ src/main/java/library/MainApp.java
@@ -0,0 +1,43 @@
+package main.java.library;
+
+import main.java.library.controller.LibraryController;
+import main.java.library.model.Book;
+import main.java.library.model.Library;
+import main.java.library.persistance.LibraryStorage;
+import main.java.library.view.LibraryPanel;
+
+import javax.swing.*;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                //caricamento libri
+                List<Book> books =LibraryStorage.loadFromFile("books.json");
+
+                //modello
+                Library library = Library.getInstance();
+                library.setBooks(books);
+
+                //controller
+                LibraryController controller = new LibraryController();
+
+                //gui
+                LibraryPanel panel = new LibraryPanel(controller);
+                controller.addObserver(panel);
+
+                //visualizzazione
+                JFrame frame = new JFrame("Library Manager");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setContentPane(panel);
+                frame.setSize(900, 600);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
+            } catch (Exception e){
+                JOptionPane.showMessageDialog(null, "Errore durante l'avvio: "+e.getMessage());
+            }
+        });
+    }
+}
