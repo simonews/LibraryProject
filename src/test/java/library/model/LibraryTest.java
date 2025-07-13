b794d86675f9a1f9675d26c5762ed84b2eca1148
simonews <simonegoffredows@gmail.com>
2025-07-13 19:51:09 +0200

    ultimi accorgimenti

--- src/test/java/library/model/LibraryTest.java
+++ src/test/java/library/model/LibraryTest.java
@@ -6,6 +6,7 @@ import main.java.library.model.BookFactory;
 import main.java.library.model.Library;
 import main.java.library.model.enums.Rating;
 import main.java.library.model.enums.ReadingStatus;
+import main.java.library.util.SortByYear;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -57,7 +58,7 @@ public class LibraryTest {
     @Test
     public void testGetAllBooks() {
         Book b1 = BookFactory.createBook("titolo1", "autore", "thriller", 2010, "1234567890", Rating.DUE, ReadingStatus.DA_LEGGERE, "aaa" );
-        Book b2 = BookFactory.createBook("titolo2", "autore", "thriller", 2010, "1234567890", Rating.DUE, ReadingStatus.DA_LEGGERE, "aaa" );
+        Book b2 = BookFactory.createBook("titolo2", "autore", "thriller", 2010, "1234567891", Rating.DUE, ReadingStatus.DA_LEGGERE, "aaa" );
         library.addBook(b1);
         library.addBook(b2);
 
@@ -79,4 +80,33 @@ public class LibraryTest {
         List<Book> filtered = controller.filterBooks("autore", "a");
         assertEquals(1, filtered.size(), "il filtro per autore non ha restituito il numero corretto di libri");
     }
+
+    @Test
+    void testUpdateBook() {
+        Book book = BookFactory.createBook("titolo1", "autore", "genere", 2005, "1224567890", Rating.DUE, ReadingStatus.DA_LEGGERE, "" );
+        controller.addBook(book);
+        int id = book.getId();
+
+        Book updatedBook = BookFactory.createBook("titoloAggiornato", "autore", "genere", 2005, "1224567890", Rating.DUE, ReadingStatus.DA_LEGGERE, "desc" );
+        boolean result = controller.updateBook(id, updatedBook);
+
+        assertTrue(result);
+        assertEquals("titoloAggiornato", library.getBookById(id).getTitle() );
+    }
+
+    @Test
+    void testSortByYear() {
+        Book b1 = BookFactory.createBook("titolo1", "a", "thriller", 2000, "1234567891", Rating.DUE, ReadingStatus.DA_LEGGERE, "aaa" );
+        Book b2 = BookFactory.createBook("titolo2", "b", "thriller", 2010, "1234567892", Rating.DUE, ReadingStatus.DA_LEGGERE, "aaa" );
+
+        controller.addBook(b1);
+        controller.addBook(b2);
+
+        controller.sortBooks(new SortByYear());
+        List<Book> sorted = controller.getAllBooks();
+
+        assertEquals("titolo1", sorted.get(0).getTitle());
+        assertEquals("titolo2", sorted.get(1).getTitle());
+    }
+
 }
